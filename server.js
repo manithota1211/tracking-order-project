91db03025d305f59b5d8ca88529a4865565f9d29
manithota1211 <thotamani39@gmail.com>
2025-11-11 17:35:25 +0530

    changed the version tag

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection
-mongoose.connect('mongodb://mongodb-service:27017/parceltracker')
+mongoose.connect('mongodb://localhost:27017/parceltracker')
 .then(async () => {
   console.log('MongoDB connected successfully');
   // Initialize chatbot FAQs
